753f148c6bd015e0425245db31c89f8b2b498a4c
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-06-24 13:37:24 +0200

    Update neo.js

--- commands/neo.js
+++ commands/neo.js
@@ -2,7 +2,7 @@ const discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
-    return message.channel.send("als je nu niet stopt ruk ik je kop eraf");
+    return message.channel.send("JaJa je spreekt hier tegen koning carnaval, en als je nu niet stopt ruk ik je kop eraf! \n\n de server die denkt: **WTH/WTF**");
 
 }
 
