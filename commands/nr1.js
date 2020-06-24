59a76c83d3e3b9b5fd482807a8e1516b461c3c62
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-06-24 19:08:34 +0200

    Update nr1.js

--- commands/nr1.js
+++ commands/nr1.js
@@ -2,7 +2,7 @@ const discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
-    if (!hasRole("Jose") || hasRole("Skittle")) return sendMessage("Sorry, U bent helaas niet welkom hier.\n\nMet vriendelijke groet\nbewakers");
+    if (message.member.roles.cache.has("Jose") || message.member.roles.cache.has("Skittle")) return sendMessage("Sorry, U bent helaas niet welkom hier.\n\nMet vriendelijke groet\nbewakers");
 
     return message.channel.send("Prive zaken, dus niet de jouwe");
 
