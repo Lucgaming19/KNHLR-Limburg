7d75f8ea24131c62a4d22c85ac160bf487b88b17
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-06-24 10:42:18 +0200

    init

--- commands/jose.js
+++ commands/jose.js
@@ -2,7 +2,7 @@ const discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
-    return message.channel.send("ruth heeft mijn kroketten gejat");
+    return message.channel.send("Jos\u00E9 is te vinden op nummer 1, laat haar met rust en stoor haar niet.");
 
 }
 
--- commands/luc.js
+++ commands/luc.js
@@ -2,7 +2,7 @@ const discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
-    return message.channel.send("Luc is de hoofd beheerder van deze discord hij is aardig maar kan ook streng wezen");
+    return message.channel.send("Luc is de beheerder van deze discord bot. Als er wat mis mee is ga dan naar hem toe.");
 
 }
 
