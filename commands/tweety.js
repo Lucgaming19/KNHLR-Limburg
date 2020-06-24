d04e03928de5b7315f1b3f9d63ea4cfafb910c5b
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-06-24 13:59:24 +0200

    Create tweety.js

---
+++ commands/tweety.js
@@ -0,0 +1,12 @@
+const discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+
+    return message.channel.send("*Mobiel gaat af* \n\n *kijkt* \n\n *ziet de tweet van tweety*");
+
+}
+
+module.exports.help = {
+    name: "tweety"
+
+}
