acf5e0d489c28c00f130db9f8d6c1910019c2a33
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-06-24 20:47:49 +0200

    Update boost.js

--- commands/boost.js
+++ commands/boost.js
@@ -2,7 +2,7 @@ const discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
-    var args = message.content.slice(prefix.length).split(/ +/);
+   // var args = message.content.slice(prefix.length).split(/ +/);
     if (!args[0]) return message.reply("no username found!");
     var boostUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(arg[0]));
     var embed = new discord.MessageEmbed()
