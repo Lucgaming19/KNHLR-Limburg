30e5ff0448a6277f333a1fc2bd1fadfb891b6db6
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-06-24 13:34:07 +0200

    Create neo.js

---
+++ commands/neo.js
@@ -0,0 +1,12 @@
+const discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+
+    return message.channel.send("als je nu niet stopt ruk ik je kop eraf");
+
+}
+
+module.exports.help = {
+    name: "neo"
+
+}
