10cc449231996bde9451b5cd2d0d485597196d0c
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-06-25 17:25:13 +0200

    Update removeuser.js

--- commands/removeuser.js
+++ commands/removeuser.js
@@ -2,14 +2,15 @@
 
 module.exports.run = async (bot, message, args) => {
 
-    const categoryID = "688478364163506220";
+	const categoryID = "688478364163506220";
+	const settedparent = message.channel.id
 
 	if (!message.member.roles.cache.has("688478365635838038")) return message.channel.send("U heeft niet de juiste rollen om dit te doen.");
 	if (!args[0]) return message.reply("no username found!");
 	var kickUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(arg[0]));
 
 	if (message.channel.parentID == categoryID) {
-		parentID.channel.message.updateOverwrite(message.guild.members.cache.find(x => x.name === `${kickUser}`), {
+		settedparent.updateOverwrite(message.guild.members.cache.find(x => x.name === `${kickUser}`), {
 			SEND_MESSAGES: false,
 			VIEW_CHANNEL: false
 		});
