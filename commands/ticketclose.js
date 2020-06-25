177afd1d9bbe004139398de2d24ecdebae42bdcb
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-06-25 16:37:29 +0200

    Update ticketclose.js

--- commands/ticketclose.js
+++ commands/ticketclose.js
@@ -1,4 +1,4 @@
-const discord = require("discord.js")
+﻿const discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
@@ -7,13 +7,37 @@ module.exports.run = async (bot, message, args) => {
     if (!message.member.roles.cache.has("688478365635838038")) return message.channel.send("U heeft niet de juiste rollen om dit te doen.");
 
     if (message.channel.parentID == categoryID) {
-        message.channel.delete(10000);
+        message.channel.delete();
+
+        var embedPromt = new discord.MessageEmbed()
+		.setColor("GREEN")	
+		.setTitle("React with in 30sec.")
+		.setDescription(`Wil je deze ${message.channel.name} sluiten?`);
+
+
+	message.channel.send(embedPromt).then(async msg => {
+
+		var emoji = await promptMessage(msg, message.author, 30, ["✅", "❌"]);
+
+		if (emoji === "✅") {
+
+			message.channel.delete();
+
+		} else if (emoji === "❌") {
+
+			msg.delete();
+
+			message.reply("Het sluiten is gecanseld").then(m => m.delete(5000));
+
+		}
+
+	})
 
         var embedCreateTicket = new discord.MessageEmbed()
             .setTitle("Ticket, " + message.channel.name)
             .setDescription(`Deze ticket Is afgerond door: ${message.author}`)
             .setFooter("Ticket is gesloten. \u00A9 Limburg")
-            .setTimestamp();
+            .setTimestamp()
 
         var ticketChannel = message.member.guild.channels.cache.find(channel => channel.name === "logs");
         if (!ticketChannel) return message.reply("Kanaal bestaat niet");
@@ -25,6 +49,23 @@ module.exports.run = async (bot, message, args) => {
         message.channel.send("U kunt dit alleen in een ticket doen.");
 
     }
+}
+async function promptMessage(message, author, time, reactions) {
+	// We gaan eerst de tijd * 1000 doen zodat we seconden uitkomen.
+	time *= 1000;
+
+	// We gaan ieder meegegeven reactie onder de reactie plaatsen.
+	for (const reaction of reactions) {
+		await message.react(reaction);
+	}
+
+	// Als de emoji de juiste emoji is die men heeft opgegeven en als ook de auteur die dit heeft aangemaakt er op klikt
+	// dan kunnen we een bericht terug sturen.
+	const filter = (reaction, user) => reactions.includes(reaction.emoji.name) && user.id === author.id;
+
+	// We kijken als de reactie juist is, dus met die filter en ook het aantal keren en binnen de tijd.
+	// Dan kunnen we bericht terug sturen met dat icoontje dat is aangeduid.
+	return message.awaitReactions(filter, { max: 1, time: time }).then(collected => collected.first() && collected.first().emoji.name);
 }
 
 module.exports.help = {
