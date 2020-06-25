65b4ea1386eab1ee4ece7f097e7edc37da1db3e1
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-06-25 14:27:45 +0200

    Update ticketclose.js

--- commands/ticketclose.js
+++ commands/ticketclose.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
 
     const categoryID = "688478364163506220";
 
-    if (!message.member.roles.cache.has('688478365635838038')) return message.channel.send("U heeft niet de juiste rollen om dit te doen.");
+    if (!guildMember.roles.has('688478365635838038')) return message.channel.send("U heeft niet de juiste rollen om dit te doen.");
 
     if (message.channel.parentID == categoryID) {
         message.channel.delete();
