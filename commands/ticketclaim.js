1e301821410a9b9eda4e848026bb1d9e68b51ae8
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-06-25 15:24:22 +0200

    init

--- commands/ticketclaim.js
+++ commands/ticketclaim.js
@@ -7,19 +7,12 @@ module.exports.run = async (bot, message, args) => {
     if (!message.member.roles.cache.has("688478365635838038")) return message.channel.send("U heeft niet de juiste rollen om dit te doen.");
 
     if (message.channel.parentID == categoryID) {
-        message.channel.delete();
-
-        var embedCreateTicket = new discord.MessageEmbed()
+               var embedCaimTicket = new discord.MessageEmbed()
             .setTitle("Ticket, " + message.channel.name)
-            .setDescription(`Deze ticket Is afgerond door: ${message.author}`)
-            .setFooter("Ticket is gesloten. \u00A9 Limburg")
+            .setDescription(`Deze ticket Is Geclaimed door: ${message.author}`)
+            .setFooter("\u00A9 Limburg")
             .setTimestamp();
-
-        var ticketChannel = message.member.guild.channels.cache.find(channel => channel.name === "logs");
-        if (!ticketChannel) return message.reply("Kanaal bestaat niet");
-
-        ticketChannel.send(embedCreateTicket);
-
+        
     } else {
 
         message.channel.send("U kunt dit alleen in een ticket doen.");
@@ -28,6 +21,5 @@ module.exports.run = async (bot, message, args) => {
 }
 
 module.exports.help = {
-    name: "close"
-
+    name: "claim"
 }
