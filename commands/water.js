83f019aaa9c1ad6d897424a8031c0bec7f1cb0da
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-06-26 09:11:54 +0200

    Update water.js

--- commands/water.js
+++ commands/water.js
@@ -3,12 +3,18 @@ const discord = require("discord.js")
 module.exports.run = async (bot, message, args) => {
 
     return message.channel.send("Een hele grote fles water graag.");
-    setTimeout("*Fles water wordt op tafel gezet*", delay[10000]);
-    message.channel.send("*fles water wordt op de tafel gezet*");
-    wait(5);
-    message.channel.send("*Pakt de fles en dringt hem op*");
-    wait(5);
-    message.channel.send("Is er meer?");
+    function vertraging1() {
+        message.channel.send("* fles water wordt op de tafel gezet *");
+    }
+    function vertraging2() {
+        message.channel.send("*Pakt de fles en dringt hem op*");
+    }
+    function vertraging3() {
+        message.channel.send("Is er meer?");
+    }
+    setTimeout(vertraging1, 1500);
+    setTimeout(vertraging2, 1500);
+    setTimeout(vertraging3, 1500);
 }
 
 module.exports.help = {
