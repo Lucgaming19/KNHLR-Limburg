0d927274096ddb10b87aec36fc792f7b58f6abc6
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-06-26 12:31:35 +0200

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -67,7 +67,7 @@ module.exports.run = async (bot, message, args) => {
                     });
 
                     var embedParent = new discord.MessageEmbed()
-                        .setColor("blue")
+                        .setColor("#3498DB")
                         .setTitle(`Hallo ${message.author.username},`)
                         .setDescription(`Het Support team help u zo snel mogelijk veder. Reden: ${reason}`)
                         .setFooter("\u00A9 Limburg")
