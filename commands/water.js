01a543767fad24740794666ce4eecef07afd38df
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-06-26 09:38:47 +0200

    init

--- commands/water.js
+++ commands/water.js
@@ -2,23 +2,10 @@ const discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
-    return message.channel.send("Een hele grote fles water graag.");
-
-    var textchannel = message.channel;
-
-    var message1 = ("* fles water wordt op de tafel gezet *");
-    var message2 = ("*Pakt de fles en dringt hem op*");
-    var message3 = ("Is er meer?");
-
-    setTimeout(() => {
-        textchannel.send(message1);
-    }, 3000);
-    setTimeout(() => {
-        textchannel.send(message2);
-    }, 3000);
-    setTimeout(() => {
-        textchannel.send(message3);
-    }, 3000);
+    return message.channel.send("Een hele grote fles water graag");
+    message.channel.send("*fles water wordt op de tafel gezet*");
+    message.channel.send("*Pakt de fles en dringt hem op*");
+    message.channel.send("Is er meer?");
 }
 
 module.exports.help = {
