699386ec156f4b80d17b075980d5887d534bb730
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-08-08 12:03:05 +0200

    init

--- commands/henri.js
+++ commands/henri.js
@@ -2,7 +2,7 @@ const discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
-    return message.channel.send("oh dus je wilt ontslag? ik kan er voor zorgen dat je dubbel ontslag krijgt :) ");
+    return message.channel.send("Hier heb je ontslag xd");
 }
 
 module.exports.help = {
--- commands/nino.js
+++ commands/nino.js
@@ -2,7 +2,7 @@ const discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
-    return message.channel.send("brb *ik ga maya even leren skateboarden*");
+    return message.channel.send(": Neo is mijn grooste vijand hierbij mijn bericht : NEEM ONTSLAG");
 }
 
 module.exports.help = {
