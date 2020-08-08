deedc7742adc9251660b643bba7533ee0cffde44
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-08-08 12:10:56 +0200

    Create 24.js

---
+++ commands/24.js
@@ -0,0 +1,16 @@
+const discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+
+    const categoryID = "688478364163506220";
+    const settedparent = message.channel.id
+    const channelName = message.channel.name
+    if (message.channel.parentID == categoryID) {
+        return message.channel.send("**__binnen de 24 uur niet reageren word dit ticket gesloten!__**");
+    }
+}
+
+module.exports.help = {
+    name: "24"
+
+}
