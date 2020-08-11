b8c86bd53b834040467afef924e5f4f8eba9a55e
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2020-08-11 13:02:44 +0200

    Create max.js

---
+++ commands/max.js
@@ -0,0 +1,11 @@
+const discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+
+    return message.channel.send("met de russische maffia leider OPFLIKKEREN OF IK RUK JE KOP ER AF");
+}
+
+module.exports.help = {
+    name: "max"
+
+}
