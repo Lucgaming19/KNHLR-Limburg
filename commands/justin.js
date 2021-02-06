73d0aa98f6d725d7ee55abf40c41616089a00bd9
Lucgaming19 <61281925+Lucgaming19@users.noreply.github.com>
2021-02-06 18:51:33 +0100

    init

--- commands/justin.js
+++ commands/justin.js
@@ -2,7 +2,9 @@ const discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
-    return message.channel.send("Ohh dus je ging niet luisteren naar de regels?? dan zal ik je het eens komen vertellen.\n\n1. NIET OKKEN!!!\n\n2. MAAK GEEN RUZIE!!!\n\n3. COC + is te druk om het gezeik op te lossen dus ga vooral niet naar hen toe: woman_tipping_hand_tone4:");
+    return message.channel.send("Is de bot offline? \nALWEER!");
+    return message.channel.sen("Laat me bouwen met Kjetil");
+    msg.delete();
 
 }
 
